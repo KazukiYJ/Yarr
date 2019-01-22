113b53df812b1c7f2af6063093024972ac29b541
Kazuki Yajima <kazuki.yajima@cern.ch>
2019-01-23 04:31:30 +0900

    Implement rescanning-only mode onto rescan_spec.sh

--- src/kernel/rescan_spec.sh
+++ src/kernel/rescan_spec.sh
@@ -6,15 +6,27 @@ DEVICES=`lspci | grep $SEARCH_WORD | sed -e "s/\ .*//g" | xargs` # Remove after 
 DEVICE_NUM=`echo $DEVICES | wc -w`
 
 if [ $DEVICE_NUM -eq 0 ]; then
-	echo "ERROR : Not found spec devices." >&2
-	exit 1
+	echo "WARNING : Not found spec devices." >&2
+	echo "Try rescanning?" >&2
+	select CHOICE in "YES" "NO"; do
+		if [ "$CHOICE" = "YES" ]; then
+			echo "Rescanning..."
+			echo "1" > /sys/bus/pci/rescan
+			echo "-----> Done!"
+			exit 0
+		else
+			echo "ByeBye!" >&2
+			exit 1
+		fi
+	done
+
 elif [ $DEVICE_NUM -eq 1 ]; then
 	DEVICE=$DEVICES
 	echo "1 device ($DEVICE) is detected."
 elif [ $DEVICE_NUM -ge 2 ]; then
 	echo "Multiple devices are detected."
 	echo "Choose a device to remove/rescan."
-  select DEVICE in $DEVICES; do
+	select DEVICE in $DEVICES; do
 		if [ -z "$DEVICE" ]; then
 			echo "WARNING : Invalid choice." >&2
 		else
